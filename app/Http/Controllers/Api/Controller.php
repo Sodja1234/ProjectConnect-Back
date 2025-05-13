7bb08ca6fd4e5a04f2432bf7765f5eabdb60b2da
line <linemangoma@gmail.com>
2025-05-13 17:07:58 +0100

    implementation de swagger

---
+++ app/Http/Controllers/Api/Controller.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller as MainController;
+
+class Controller extends MainController
+{
+    //
+
+    /**
+     * @OA\Get(
+     *     path="/api/register",
+     *     summary="Register a new user",
+     *     @OA\Parameter(
+     *         name="name",
+     *         in="query",
+     *         description="User's name",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Parameter(
+     *         name="email",
+     *         in="query",
+     *         description="User's email",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Parameter(
+     *         name="password",
+     *         in="query",
+     *         description="User's password",
+     *         required=true,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(response="201", description="User registered successfully"),
+     *     @OA\Response(response="422", description="Validation errors")
+     * )
+     */
+    public function register(Request $request)
+    {
+        return response()->json(['message' => 'User registered successfully'], 201);
+    }
+}
