1b81d5b69c678b23c3affca0901e0f1a1a132af5
Labeya <laurentmwn@gmail.com>
2025-05-27 18:07:45 +0100

    Création d'un utilisateur

---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+use App\Events\RegisteredUserEvent;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+
+class RegisterController extends Controller
+{
+
+    public function __invoke(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+
+        if ($validation->fails()) {
+            return response()->json([
+                'status' => HttpResponse::HTTP_UNPROCESSABLE_ENTITY,
+                'errors' => $validation->errors()
+            ]);
+        }
+
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->string('password')),
+        ]);
+
+        event(new RegisteredUserEvent($user));
+
+        return response()->noContent();
+    }
+}
--- routes/api-auth.php
+++ routes/api-auth.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('/login', AuthController::class);
+Route::post('/register', RegisterController::class);
