b07752f5bf20f316a27afaaa8127b02fc7ad1189
Labeya <laurentmwn@gmail.com>
2025-05-27 18:55:00 +0100

    Réinitialisation de mot de passe

--- routes/api-auth.php
+++ routes/api-auth.php
@@ -1,9 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\Auth\RegisterController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\NewPasswordController;
+use App\Http\Controllers\Api\Auth\PasswordResetLinkController;
 
 
 Route::post('/login', AuthController::class);
 Route::post('/register', RegisterController::class);
+Route::post('/forgot-password', PasswordResetLinkController::class);
+Route::post('/reset-password', NewPasswordController::class);
