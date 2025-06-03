6800f6a3c8bb2cfaecb9b25ca3b5fe2b93443f46
line <linemangoma@gmail.com>
2025-06-03 15:22:43 +0100

    ajout de la relation skills() many-to-many dans le modèle Portfolio

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Skill extends Model
 {
     protected $fillable = ['name'];
+     public function portfolio()
+    {
+        return $this->belongsToMany(portfolio::class);
+    }
+public function skills()
+    {
+        return $this->belongsToMany(
+            Skill::class,
+            'portfolio_skill'
+        );
+    }
 }
