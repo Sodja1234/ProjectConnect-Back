c9244c033fdec97002819eda03e2fb70239fa9b5
line <linemangoma@gmail.com>
2025-06-05 17:00:32 +0100

    mise à jour des routes API pour Portfolio

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,16 @@
 <?php
 
-use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Controller;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\SkillController;
 use App\Http\Controllers\DomainController;
+use App\Http\Controllers\API\ChatController;
+use App\Http\Controllers\CandidacyController;
 use App\Http\Controllers\PortfolioController;
+use App\Http\Controllers\API\MessageController;
+use App\Http\Controllers\Api\ProjectController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
