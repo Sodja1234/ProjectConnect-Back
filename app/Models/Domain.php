14e531291532bdb11bdc4d4da729fcf221ca5676
line <linemangoma@gmail.com>
2025-07-15 13:39:53 +0100

    Documentation des models Candidacy, chat, domain, experience

--- app/Models/Domain.php
+++ app/Models/Domain.php
@@ -4,6 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *   schema="Domain",
+ *   required={"name"},
+ *   @OA\Property(
+ *     property="id",
+ *     type="integer",
+ *     readOnly=true,
+ *     description="Identifiant unique du domaine"
+ *   ),
+ *   @OA\Property(
+ *     property="name",
+ *     type="string",
+ *     description="Nom du domaine"
+ *   ),
+ *   @OA\Property(
+ *     property="created_at",
+ *     type="string",
+ *     format="date-time",
+ *     readOnly=true,
+ *     description="Date de création du domaine"
+ *   ),
+ *   @OA\Property(
+ *     property="updated_at",
+ *     type="string",
+ *     format="date-time",
+ *     readOnly=true,
+ *     description="Date de dernière mise à jour du domaine"
+ *   )
+ * )
+ */
+
 class Domain extends Model
 {
     protected $fillable = ["name"];
