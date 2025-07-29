ff91259d3213c0adc86c457f79a851438b030dfc
Impulse <ephraimimpulse17@gmail.com>
2025-07-29 19:32:31 +0100

    ajout des relations dans le model chat et project

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -39,5 +39,10 @@ class Chat extends Model
     {
         return $this->hasOne(Message::class)->latestOfMany();
     }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
 
