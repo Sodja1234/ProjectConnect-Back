c08787d3a1bb0a1c14ed6dfc2d47ce7d7bbb9155
Labeya <laurentmwn@gmail.com>
2025-07-31 14:17:47 +0100

    renommage de la colonne 'availlability' en 'is_availlability' (convention de laravel pour les champs de type bool)

---
+++ database/migrations/2025_07_31_130919_update_column_is_availlability_profile_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->renameColumn('availability', 'is_availability');
+            $table->boolean('is_availability')
+                ->default(false)
+                ->nullable()
+                ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_availability')->change();
+            $table->renameColumn('is_availability', 'availability');
+        });
+    }
+};
